9d7f0d78ab747ea1064af453475a0d8bd229b7bf
geniruphin-junior <geniruphin@gmail.com>
2026-07-20 10:48:24 +0200

    fix:import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from math import MathSuperEngine
+from math_moteur import MathSuperEngine
 
 print("======== Bienvenue dans le moteur scientifique =========\nchoisis une option : ")
 menu = "1.Algèbre\n2.Trigonometrie\n3.Stat\n4.calcul simple\n5.Retour"
