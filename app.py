61805eb375786f22bc63ad8dc482b5ef3e7fad32
geniruphin-junior <geniruphin@gmail.com>
2026-08-01 13:08:53 +0200

    boucle while

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 from math_moteur import MathSuperEngine
 import sys, os
-from array_stat import exec_stat
+from array_stat import exec_stat as stat
 
 print("======== Bienvenue dans le moteur scientifique =========\n")
 menu = "1.Algèbre\n2.Trigonometrie\n3.Stat\n4.calcul simple\n5.Retour"
-
 print(menu)
-choise_user = input("quelle est votre option : ")
-if choise_user == "1" or choise_user in [
-    "Algebre",
-    "algebre",
-    "algebra",
-    "algèbre",
-]:
-    print("Metter ici votre équation")
-elif choise_user == "2" or choise_user in ["Trigo", "trigonometrie", "trigo"]:
-    print("mettez ici votre expression trigonometrique")
-elif choise_user == "3" or choise_user in ["stat", "statistiques", "statistique"]:
-    print("mettez ici votre jeu de données en forme ")
-    liste = input("Poser ici votre liste : ")
-    print(exec_stat(liste))
+while True:
+    choise_user = input("quelle est votre option : ")
+    if choise_user == "5":
+        print("arret du processus")
+        break
+    elif choise_user == "1" or choise_user in [
+        "Algebre",
+        "algebre",
+        "algebra",
+        "algèbre",
+    ]:
+        print("Metter ici votre équation")
+    elif choise_user == "2" or choise_user in ["Trigo", "trigonometrie", "trigo"]:
+        print("mettez ici votre expression trigonometrique")
+    elif choise_user == "3" or choise_user in ["stat", "statistiques", "statistique"]:
+        print("mettez ici votre jeu de données en forme ")
+        liste = input("Poser ici votre liste : ")
+        print(exec_stat(liste))
 
-elif choise_user == "4":
-    print("mets tes calculs")
-else:
-    print("Cette fonctionnalité n'est pas reconnue par le moteur")
+    elif choise_user == "4":
+        print("mets tes calculs")
+    else:
+        print("Cette fonctionnalité n'est pas reconnue par le moteur")
